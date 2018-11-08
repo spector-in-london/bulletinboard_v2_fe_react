85a100c6008b1b4eb5b4e13f90c6788a3b465b89
robertschaap <robert.schaap@mac.com>
2018-11-08 23:31:56 +0100

    server.py: setup base comments route

--- server.py
+++ server.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello Cruel World"
+
+@app.route("/api/comments", methods=["GET", "POST"])
+def comments():
+    if request.method == "POST":
+        return ""
+    else:
+        return "Comments"
+
