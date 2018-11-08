76c0d132e64f6608d30b2870116eb38f6e952049
robertschaap <robert.schaap@mac.com>
2018-11-08 23:10:03 +0100

    App: drafted skeleton layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const ContentWrapper = styled.div`
+  width: 80%;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  text-align: right;
+`;
+
+const Main = styled.main`
+  background-color: #fafafa;
+`;
+
+const SortButton = styled.button``;
+const LoadMoreButton = styled.button``;
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Bulletin Board
-      </div>
+      <ContentWrapper>
+        <Header>
+          <div>bulletin board</div>
+          <nav>
+            <a href='/'>read</a>
+            <a href='/'>write</a>
+          </nav>
+          <div />
+        </Header>
+        <Main>
+          <SortButton>sort: newest first</SortButton>
+          <div>
+            <div>
+              <div>
+                <div>Header Left</div>
+                <div>Date</div>
+              </div>
+              <div>Body</div>
+            </div>
+          </div>
+          <LoadMoreButton>load more...</LoadMoreButton>
+        </Main>
+      </ContentWrapper>
     );
   }
 }
