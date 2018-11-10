05b011b49a9d10506650637cb6367b4f77fefa85
robertschaap <robert.schaap@mac.com>
2018-11-10 13:57:53 +0100

    ContentWrapper: added media query for width

--- src/components/ContentWrapper.js
+++ src/components/ContentWrapper.js
@@ -4,6 +4,10 @@ import styled from 'styled-components';
 const ContentWrapper = styled.div`
   width: 80%;
   margin: 0 auto;
+
+  @media (min-width: 1250px) {
+    width: 1000px;
+  }
 `;
 
 export default ContentWrapper;
