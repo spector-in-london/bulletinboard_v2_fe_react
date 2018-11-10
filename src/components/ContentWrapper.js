8cf60c6ed9239af09ac00e01462702dff4610bc1
robertschaap <robert.schaap@mac.com>
2018-11-10 14:01:59 +0100

    ContentWrapper: added flexbox

--- src/components/ContentWrapper.js
+++ src/components/ContentWrapper.js
@@ -3,7 +3,10 @@ import styled from 'styled-components';
 
 const ContentWrapper = styled.div`
   width: 80%;
+  min-height: 100vh;
   margin: 0 auto;
+  display: flex;
+  flex-direction: column;
 
   @media (min-width: 1250px) {
     width: 1000px;
