d7dc2180c0068f5fd8f2ea170e46ef7298340862
robertschaap <robert.schaap@mac.com>
2018-11-11 11:07:06 +0100

    index.js: fixed linting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import "./styles/normalize.css";
+import './styles/normalize.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
