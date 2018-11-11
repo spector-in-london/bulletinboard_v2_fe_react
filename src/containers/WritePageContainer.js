21999e10ebee5c5ea9edf2108dc423233e1e63be
robertschaap <robert.schaap@mac.com>
2018-11-11 12:24:53 +0100

    WritePageContainer: setup component

---
+++ src/containers/WritePageContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import WritePage from '../components/WritePage';
+
+class WritePageContainer extends Component {
+  render() {
+    return (
+      <WritePage />
+    );
+  }
+}
+
+export default WritePageContainer;
