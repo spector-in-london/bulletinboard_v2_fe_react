6b53c941434c722e946fb9af8939abaf18863a55
robertschaap <robert.schaap@mac.com>
2018-11-11 12:24:37 +0100

    WritePage: setup base component

---
+++ src/components/WritePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ReadPage extends Component {
+  render() {
+    return (
+      <>
+      <div>
+        WritePage
+      </div>
+      </>
+    );
+  }
+}
+
+export default ReadPage;
