4626b640ac46b72be19d5687743c99a36a3dce58
robertschaap <robert.schaap@mac.com>
2018-11-11 12:16:22 +0100

    ReadPageContainer: setup container

---
+++ src/containers/ReadPageContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import ReadPage from '../components/ReadPage';
+
+class ReadPageContainer extends Component {
+  render() {
+    return (
+      <ReadPage />
+    );
+  }
+}
+
+export default ReadPageContainer;
