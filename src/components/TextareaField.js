91cfb02f9f96d8e60c76679843f14438541d78c7
robertschaap <robert.schaap@mac.com>
2018-11-11 13:10:00 +0100

    TextAreaField: setup component

---
+++ src/components/TextareaField.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import FormGroup from './FormGroup';
+
+const Textarea = styled.textarea`
+  border: unset;
+  border-bottom: 1px solid black;
+  padding: 0.5em;
+  resize: none;
+`;
+
+const TextareaField = (props) => {
+  const { label, autofocus } = props;
+
+  return (
+    <FormGroup label={label}>
+      <Textarea rows='6' placeholder='Max 255 characters' maxLength='255' autoFocus={autofocus} />
+    </FormGroup>
+  );
+};
+
+export default TextareaField;
