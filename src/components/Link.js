2c43c3e526132dcf69d2a4d54924453ac34a607d
robertschaap <robert.schaap@mac.com>
2018-11-11 15:52:30 +0100

    Link: focus styles

--- src/components/Link.js
+++ src/components/Link.js
@@ -13,6 +13,11 @@ const Link = styled(RouterLink)`
   &:not(:last-child) {
     margin-right: 8px;
   }
+
+  :focus {
+    outline: none;
+    border-bottom: 2px solid #ffbc05;
+  }
 `;
 
 export default Link;
