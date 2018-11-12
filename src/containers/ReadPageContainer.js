9fa84c6adcbb59e79115abff83058792a2210a6a
robertschaap <robert.schaap@mac.com>
2018-11-12 23:49:02 +0100

    ReadPageContainer: added api call

--- src/containers/ReadPageContainer.js
+++ src/containers/ReadPageContainer.js
@@ -4,12 +4,20 @@ import ReadPage from '../components/ReadPage';
 
 class ReadPageContainer extends Component {
   state = {
-    comments: [
-      { id: 1, title: 'title', body: 'body', date: '11-11-2018' },
-      { id: 2, title: 'title', body: 'body', date: '11-11-2018' },
-      { id: 3, title: 'title', body: 'body', date: '11-11-2018' },
-    ]
+    comments: [],
   }
+
+  componentDidMount() {
+    this.fetchComments();
+  }
+
+  fetchComments() {
+    fetch('/api/comments/')
+      .then(res => res.json())
+      .then(comments => this.setState({ comments }))
+      .catch(console.error); // eslint-disable-line no-console
+  }
+
   render() {
     return (
       <ReadPage comments={this.state.comments}/>
