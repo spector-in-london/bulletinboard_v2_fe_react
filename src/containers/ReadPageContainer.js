d395d5f200034134152a4f4e450f5f6066e80aed
robertschaap <robert.schaap@mac.com>
2018-11-12 22:43:08 +0100

    ReadPageContainer: added dummy comments

--- src/containers/ReadPageContainer.js
+++ src/containers/ReadPageContainer.js
@@ -3,9 +3,16 @@ import React, { Component } from 'react';
 import ReadPage from '../components/ReadPage';
 
 class ReadPageContainer extends Component {
+  state = {
+    comments: [
+      { id: 1, title: 'title', body: 'body', date: '11-11-2018' },
+      { id: 2, title: 'title', body: 'body', date: '11-11-2018' },
+      { id: 3, title: 'title', body: 'body', date: '11-11-2018' },
+    ]
+  }
   render() {
     return (
-      <ReadPage />
+      <ReadPage comments={this.state.comments}/>
     );
   }
 }
