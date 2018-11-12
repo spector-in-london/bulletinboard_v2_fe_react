e4d069ce232074dd071b0a2a9557a128fbbca2d0
robertschaap <robert.schaap@mac.com>
2018-11-12 23:45:34 +0100

    server: added dummy data to comments route

--- server.py
+++ server.py
@@ -1,14 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello Cruel World"
 
-@app.route("/api/comments", methods=["GET", "POST"])
+@app.route("/api/comments/", methods=["GET", "POST"])
 def comments():
     if request.method == "POST":
         return ""
     else:
-        return "Comments"
+        response = [
+            { "id": 1, "title": "title", "body": "body", "date": "11-11-2018" },
+            { "id": 2, "title": "title", "body": "body", "date": "11-11-2018" },
+            { "id": 3, "title": "title", "body": "body", "date": "11-11-2018" },
+        ]
+        return jsonify(response)
 
