f32a8b11d87f8b7b8d106ae2c10869f671f25c98
robertschaap <robert.schaap@mac.com>
2018-11-13 22:32:29 +0100

    WritePageContainer: added state and state change handler

--- src/containers/WritePageContainer.js
+++ src/containers/WritePageContainer.js
@@ -3,9 +3,27 @@ import React, { Component } from 'react';
 import WritePage from '../components/WritePage';
 
 class WritePageContainer extends Component {
+  state = {
+    comment: {
+      title: '',
+      body: '',
+    }
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      comment: {
+        ...this.state.comment,
+        [name]: value,
+      }
+    });
+  }
+
   render() {
     return (
-      <WritePage />
+      <WritePage onChange={this.handleChange} />
     );
   }
 }
