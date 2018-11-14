3d38c38c56ed06a46a0a3c08b052dc70f337a9c5
robertschaap <robert.schaap@mac.com>
2018-11-14 23:44:42 +0100

    server: split comment routes

--- server.py
+++ server.py
@@ -5,27 +5,28 @@ app = Flask(__name__)
 def hello():
     return "Hello Cruel World"
 
-@app.route("/api/comments/", methods=["GET", "POST"])
-def comments():
-    if request.method == "POST":
-        print(request.get_json())
-        response = {
-            "status": "success",
-            "data": None,
-            "message": None,
-        }
-        return jsonify(response), 200
-    else:
-        comments = [
-            { "id": 1, "title": "title", "body": "body", "date": "11-11-2018" },
-            { "id": 2, "title": "title", "body": "body", "date": "11-11-2018" },
-            { "id": 3, "title": "title", "body": "body", "date": "11-11-2018" },
-        ]
+@app.route("/api/comments/", methods=["POST"])
+def postComments():
+    print(request.get_json())
+    response = {
+        "status": "success",
+        "data": None,
+        "message": None,
+    }
+    return jsonify(response), 200
 
-        response = {
-            "status": "success",
-            "data": { "comments": comments },
-            "message": None,
-        }
+@app.route("/api/comments/", methods=["GET"])
+def getComments():
+    comments = [
+        { "id": 1, "title": "title", "body": "body", "date": "11-11-2018" },
+        { "id": 2, "title": "title", "body": "body", "date": "11-11-2018" },
+        { "id": 3, "title": "title", "body": "body", "date": "11-11-2018" },
+    ]
 
-        return jsonify(response), 200
+    response = {
+        "status": "success",
+        "data": { "comments": comments },
+        "message": None,
+    }
+
+    return jsonify(response), 200
