7df4ebfbab1cc75a96c6a9719da6960e2a49421e
robertschaap <robert.schaap@mac.com>
2018-11-15 22:12:01 +0100

    server: added api_response functions

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
+from api import api_response, api_res_type
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,25 +9,16 @@ def hello():
 @app.route("/api/comments/", methods=["POST"])
 def postComments():
     print(request.get_json())
-    response = {
-        "status": "success",
-        "data": None,
-        "message": None,
-    }
-    return jsonify(response), 200
+    return api_response(api_res_type["success"]), 200
 
 @app.route("/api/comments/", methods=["GET"])
 def getComments():
-    comments = [
-        { "id": 1, "title": "title", "body": "body", "date": "11-11-2018" },
-        { "id": 2, "title": "title", "body": "body", "date": "11-11-2018" },
-        { "id": 3, "title": "title", "body": "body", "date": "11-11-2018" },
-    ]
-
-    response = {
-        "status": "success",
-        "data": { "comments": comments },
-        "message": None,
+    data = {
+        "comments": [
+            { "id": 1, "title": "title", "body": "body", "date": "11-11-2018" },
+            { "id": 2, "title": "title", "body": "body", "date": "11-11-2018" },
+            { "id": 3, "title": "title", "body": "body", "date": "11-11-2018" },
+        ]
     }
 
-    return jsonify(response), 200
+    return api_response(api_res_type["success"], data), 200
