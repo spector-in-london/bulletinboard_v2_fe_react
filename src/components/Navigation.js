b1ef2da4ca2b0c3f7b04c9ba1ec2dde08034bdc4
robertschaap <robert.schaap@mac.com>
2018-11-18 21:25:40 +0100

    Navigation: added media queries

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 const Navigation = styled.nav`
   font-size: 2.5em;
   margin-bottom: 1rem;
+
+  @media (max-width: 450px) {
+    font-size: 1.5em;
+  }
 `;
 
 export default Navigation;
