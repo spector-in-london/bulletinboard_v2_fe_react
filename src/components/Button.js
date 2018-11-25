ed230467360383cba6a3dde7dd46e8aee5109e15
robertschaap <robert.schaap@mac.com>
2018-11-25 16:01:19 +0100

    Button: added keyboard only focus styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const ButtonWrapper = styled.button`
   display: block;
@@ -7,23 +7,40 @@ const ButtonWrapper = styled.button`
   border: none;
   padding: 0.5em;
   background-color: rgba(255, 188, 5, 0.265);
-
-  :focus {
-    outline: none;
-    background-color: #ffbc05;
-  }
+  outline: none;
 
   :disabled {
     cursor: not-allowed;
     background-color: #eee;
   }
+
+  ${props => props.focusWithKeyboard && css`
+    :focus {
+      outline: none;
+      background-color: #ffbc05;
+      box-shadow: 0 0 0 3px rgba(255, 188, 5, 0.265)
+    }
+  `}
 `;
 
 class Button extends React.Component {
+  state = {
+    focusWithKeyboard: false,
+  }
+
+  toggleInputMethod = ({ type }) => {
+    type === 'keyup'
+      ? this.setState({ focusWithKeyboard: true })
+      : this.setState({ focusWithKeyboard: false });
+  }
+
   render() {
     return (
       <ButtonWrapper
-        {...this.props} />
+        {...this.props}
+        focusWithKeyboard={this.state.focusWithKeyboard}
+        onKeyUp={this.toggleInputMethod}
+        onMouseDown={this.toggleInputMethod} />
     );
   }
 }
