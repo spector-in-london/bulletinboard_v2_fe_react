f318744ee54f8e827ea488b751f116291ac4ec73
robertschaap <robert.schaap@mac.com>
2019-02-19 21:51:31 +0100

    database: added declerative base

--- database.py
+++ database.py
@@ -1,12 +1,16 @@
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine,
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 db_string = 'postgres://localhost:5432:bulletinboard'
 engine = create_engine(db_string, convert_unicode=True)
-metadata = MetaData()
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
 
+Base = declarative_base()
+Base.query = db_session.query_property()
+
 def init_db():
-    metadata.create_all(bind=engine)
+    import models
+    Base.metadata.create_all(bind=engine)
