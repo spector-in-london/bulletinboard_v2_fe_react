3530181bdf3b5134bf01eedd189afa7cb8436687
robertschaap <robert.schaap@mac.com>
2019-02-23 14:53:12 +0100

    Background: cleanup redundant elements

--- src/components/Background.js
+++ src/components/Background.js
@@ -3,12 +3,9 @@ import React from 'react';
 const Background = () => {
   return (
     <svg style={{ position: 'fixed', bottom: 0, zIndex: -1 }} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1920 725'>
-      <defs><clipPath id='a'><path d='M0 0h1920v725H0z'/></clipPath></defs>
-      <g data-name='Web 1920 – 1' clipPath='url(#a)'>
-        <path fill='#fff' d='M0 0h1920v725H0z'/>
-        <path data-name='Subtraction 7' d='M1920.001 727h-244.233L.001 309.184V0l1920 478.71V727z' fill='#ffbc05'/>
-        <path data-name='Subtraction 6' d='M1824.242 726.835H.001V272l1824.24 454.834z'/>
-      </g>
+      <path fill='#fff' d='M0 0h1920v725H0z'/>
+      <path d='M1920.001 727h-244.233L.001 309.184V0l1920 478.71V727z' fill='#ffbc05'/>
+      <path d='M1824.242 726.835H.001V272l1824.24 454.834z'/>
     </svg>
   );
 };
