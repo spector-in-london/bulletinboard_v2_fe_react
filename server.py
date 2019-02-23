df97fd511b4211ce8105ac197c02f3b345e4eaf5
robertschaap <robert.schaap@mac.com>
2019-02-23 14:08:42 +0100

    server: implemented crude db query

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 from flask import Flask, request
 from api import api_response, api_res_type
+from database import init_db, db_session
+from models import Post
+from json import dumps
+
 app = Flask(__name__)
+init_db()
 
 @app.route("/")
 def hello():
@@ -13,12 +18,19 @@ def postComments():
 
 @app.route("/api/comments/", methods=["GET"])
 def getComments():
+
+    result = []
+    for instance in db_session.query(Post).limit(4):
+        temp = instance.__dict__
+        temp.pop('_sa_instance_state', None)
+        result.append(temp)
+
     data = {
-        "comments": [
-            { "id": 1, "title": "title", "body": "body", "date": "11-11-2018" },
-            { "id": 2, "title": "title", "body": "body", "date": "11-11-2018" },
-            { "id": 3, "title": "title", "body": "body", "date": "11-11-2018" },
-        ]
+        "comments": result
     }
 
     return api_response(api_res_type["success"], data), 200
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove
