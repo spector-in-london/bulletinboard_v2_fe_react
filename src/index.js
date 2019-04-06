0c6cd5e066678c75c1441cbede074b6db47b69bd
robertschaap <robert.schaap@mac.com>
2019-04-06 23:23:17 +0200

    serviceworker: removed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/normalize.css';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
