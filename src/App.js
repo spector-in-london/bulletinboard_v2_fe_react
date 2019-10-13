1b3aa0b2f189f12edd5e1f0bd89b28518ba20b7e
robertschaap <robert.schaap@mac.com>
2019-10-13 20:43:26 +0200

    App: refactored to function component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import MainLayout from './components/MainLayout';
 import ReadPageContainer from './containers/ReadPageContainer';
 import WritePageContainer from './containers/WritePageContainer';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <MainLayout>
-          <Route exact path='/' component={ReadPageContainer} />
-          <Route path='/read' component={ReadPageContainer} />
-          <Route path='/write' component={WritePageContainer} />
-        </MainLayout>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <MainLayout>
+        <Route exact path='/' component={ReadPageContainer} />
+        <Route path='/read' component={ReadPageContainer} />
+        <Route path='/write' component={WritePageContainer} />
+      </MainLayout>
+    </BrowserRouter>
+  );
+};
 
 export default App;
