3a1816dfa1565e75339f128629044db00c2e75aa
robertschaap <robert.schaap@mac.com>
2019-10-13 20:42:12 +0200

    WritePage: refactored to function component

--- src/components/WritePage.js
+++ src/components/WritePage.js
@@ -1,38 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import SubmitButton from '../components/SubmitButton';
 import InputField from '../components/InputField';
 import Message from '../components/Message';
 import TextareaField from '../components/TextareaField';
 
-class WritePage extends Component {
-  render() {
-    const { onChange, onSubmit, isBusy, hasError } = this.props;
+const WritePage = (props) => {
+  const {
+    hasError,
+    isBusy,
+    onChange,
+    onSubmit,
+  } = props;
 
-    return (
-      <form onSubmit={onSubmit}>
-        <InputField
-          autofocus
-          label='Your Name'
-          name='name'
-          onChange={onChange} />
-        <InputField
-          label='A Title for Your Comment'
-          name='title'
-          onChange={onChange} />
-        <TextareaField
-          label='Your Comment'
-          name='body'
-          onChange={onChange} />
-        <SubmitButton
-          disabled={isBusy}
-          type='submit'>
-          Submit
-        </SubmitButton>
-        {hasError && <Message error/>}
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={onSubmit}>
+      <InputField
+        autofocus
+        label='Your Name'
+        name='name'
+        onChange={onChange} />
+      <InputField
+        label='A Title for Your Comment'
+        name='title'
+        onChange={onChange} />
+      <TextareaField
+        label='Your Comment'
+        name='body'
+        onChange={onChange} />
+      <SubmitButton
+        disabled={isBusy}
+        type='submit'>
+        Submit
+      </SubmitButton>
+      {hasError && <Message error/>}
+    </form>
+  );
+};
 
 export default WritePage;
