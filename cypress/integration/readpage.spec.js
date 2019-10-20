b49fe7a2bccba7af280a435643063e903debddd7
robertschaap <robert.schaap@mac.com>
2019-10-20 14:54:34 +0200

    cypress: setup readpage tests

--- cypress/integration/readpage.spec.js
+++ cypress/integration/readpage.spec.js
@@ -1,13 +1,66 @@
 import * as locators from '../locators/locators';
 
-describe('read page', () => {
-  beforeEach(() => {
+context('server error message', () => {
+  before(() => {
+    cy.server({ status: 500 });
+    cy.route('/api/comments?offset=0&sort=desc');
     cy.visit('/read');
   });
 
-  context('warning messages', () => {
-    it('should display a message if there are no comments', () => {
-      cy.get(locators.noCommentsMessage).should('be.visible');
-    });
+  it('should display an error message on server error', () => {
+    cy.get(locators.errorMessage).should('be.visible');
+  });
+});
+
+context('no comments message', () => {
+  before(() => {
+    cy.server();
+    cy.route('/api/comments?offset=0&sort=desc', 'fixture:comments-empty');
+    cy.visit('/read');
+  });
+
+  it('should display a message if there are no comments', () => {
+    cy.get(locators.noCommentsMessage).should('be.visible');
+  });
+});
+
+context('comments', () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route('/api/comments?offset=0&sort=desc', 'fixture:comments').as('comments');
+    cy.route('/api/comments?offset=1&sort=desc', 'fixture:comments-more');
+    cy.route('/api/comments?offset=0&sort=asc', 'fixture:comments');
+    cy.route('/api/comments?offset=1&sort=asc', 'fixture:comments-more');
+    cy.visit('/read');
+    cy.wait('@comments');
+  });
+
+  it('should load comments on page load', () => {
+    cy.get(locators.commentsComponent)
+      .children()
+      .its('length')
+      .should('be.gt', 0);
+  });
+
+  it('should load more comments when load more button is clicked', () => {
+    cy.get(locators.loadMoreButton)
+      .click()
+      .then(() => {
+        cy.get(locators.commentsComponent)
+          .children()
+          .its('length')
+          .should('be.gt', 0);
+      });
+  });
+
+  it('should reload comments when sort button is clicked', () => {
+    cy.get(locators.sortButton)
+      .click()
+      .then(() => {
+        cy.get(locators.commentsComponent)
+          .children()
+          .its('length')
+          .should('be.gt', 0);
+      });
   });
 });
