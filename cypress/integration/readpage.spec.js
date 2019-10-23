2a7ccdb1888f86d5c4b46bec8b4e5e573e86e8ef
robertschaap <robert.schaap@mac.com>
2019-10-23 22:46:24 +0200

    cypress: readpage: replace hardcoded routes

--- cypress/integration/readpage.spec.js
+++ cypress/integration/readpage.spec.js
@@ -1,10 +1,11 @@
 import * as locators from '../locators/locators';
+import Routes from '../../src/constants/routes';
 
 context('server error message', () => {
   before(() => {
     cy.server({ status: 500 });
     cy.route('/api/comments?offset=0&sort=desc');
-    cy.visit('/read');
+    cy.visit(Routes.READ);
   });
 
   it('should display an error message on server error', () => {
@@ -16,7 +17,7 @@ context('no comments message', () => {
   before(() => {
     cy.server();
     cy.route('/api/comments?offset=0&sort=desc', 'fixture:comments-empty');
-    cy.visit('/read');
+    cy.visit(Routes.READ);
   });
 
   it('should display a message if there are no comments', () => {
@@ -31,7 +32,7 @@ context('comments', () => {
     cy.route('/api/comments?offset=1&sort=desc', 'fixture:comments-more');
     cy.route('/api/comments?offset=0&sort=asc', 'fixture:comments');
     cy.route('/api/comments?offset=1&sort=asc', 'fixture:comments-more');
-    cy.visit('/read');
+    cy.visit(Routes.READ);
     cy.wait('@comments');
   });
 
