ff9f377005d6472e38742ad7bd23d5a7749e7548
robertschaap <robert.schaap@mac.com>
2019-10-26 12:56:57 +0200

    hooks: move useContentCopy to own file

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,35 +1,5 @@
-import React, { createContext, useContext } from "react";
+import { useContentCopy } from './useContentCopy';
 
-interface stateType {
-  [key: string]: string;
+export {
+  useContentCopy,
 }
-
-const contentCopy: stateType = Object.freeze({
-  "comments.beFirst": "Nothing yet, be the first!",
-  "comments.loadMore": "load more...",
-  "formfield.maxLength255": "Max 255 characters",
-  "formfield.maxLength50": "Max 50 characters",
-  "message.broken": "Potentially, things are broken. Yell at your screen and try again.",
-  "site.navigation.read": "read",
-  "site.navigation.write": "write",
-  "site.title": "bulletin board",
-  "sort.newestFirst": "sort: newest first",
-  "sort.oldestFirst": "sort: oldest first",
-  "writePageForm.label.body": "Your Comment",
-  "writePageForm.label.name": "Your Name",
-  "writePageForm.label.title": "A Title for Your Comment",
-  "writePageForm.submit": "Submit",
-});
-
-const ContentContext = createContext<stateType>(contentCopy);
-
-export const useContentCopy = (messageId: string): string => {
-  const copy = useContext(ContentContext);
-
-  if (copy && copy[messageId]) {
-    return copy[messageId];
-  }
-
-  console.warn(`useContentCopy: messageId: ${messageId} not found, falling back to raw message`);
-  return '';
-};
