1d8d528890b82dbb51275521bfef0202f1826d2c
robertschaap <robert.schaap@mac.com>
2019-10-26 12:49:08 +0200

    contentCopy: sort keys

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -7,18 +7,18 @@ interface stateType {
 const contentCopy: stateType = Object.freeze({
   "comments.beFirst": "Nothing yet, be the first!",
   "comments.loadMore": "load more...",
+  "formfield.maxLength255": "Max 255 characters",
+  "formfield.maxLength50": "Max 50 characters",
   "message.broken": "Potentially, things are broken. Yell at your screen and try again.",
   "site.navigation.read": "read",
   "site.navigation.write": "write",
   "site.title": "bulletin board",
   "sort.newestFirst": "sort: newest first",
   "sort.oldestFirst": "sort: oldest first",
+  "writePageForm.label.body": "Your Comment",
   "writePageForm.label.name": "Your Name",
   "writePageForm.label.title": "A Title for Your Comment",
-  "writePageForm.label.body": "Your Comment",
   "writePageForm.submit": "Submit",
-  "formfield.maxLength50": "Max 50 characters",
-  "formfield.maxLength255": "Max 255 characters",
 });
 
 const ContentContext = createContext<stateType>(contentCopy);
