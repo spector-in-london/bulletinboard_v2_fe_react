a2dbf54f9f9b64fef621413b9cfb88249e3803b1
robertschaap <robert.schaap@mac.com>
2019-10-26 11:48:53 +0200

    routes: add api routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -2,6 +2,9 @@ const Routes = {
   HOME: '/',
   READ: '/read',
   WRITE: '/write',
+
+  API_GET_COMMENTS: '/api/comments?offset=:offset&sort=:sort',
+  API_POST_COMMENTS: '/api/comments',
 };
 
 export default Routes;
