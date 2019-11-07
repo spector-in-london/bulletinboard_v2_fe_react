f6eae89bca205f0c63ef287b64a3c375cbb388d8
robertschaap <robert.schaap@mac.com>
2019-11-07 21:40:38 +0100

    theme: add typography

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,10 +2,15 @@ const spacing = (number: number): string => {
   return (number * 4).toString() + "px";
 };
 
+const typography = {
+  fontFamily: `'Oxygen', 'Helvetica Neue', sans-serif`,
+}
+
 const theme = {
   color: {
     black: 'black',
   },
+  typography,
   spacing,
 };
 
