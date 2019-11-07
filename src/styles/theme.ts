c0e3a822af88ae5dafe5ab5e4d09365011e04353
robertschaap <robert.schaap@mac.com>
2019-11-07 21:34:29 +0100

    theme: setup spacing mixin

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,12 @@
+const spacing = (number: number): string => {
+  return (number * 4).toString() + "px";
+}
+
 const theme = {
   color: {
     black: 'black',
   },
+  spacing,
 };
 
 export default theme;
