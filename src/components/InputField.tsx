be26d2cc19dd1877229779db926aa6fec0d7a01c
robertschaap <robert.schaap@mac.com>
2019-11-12 19:40:13 +0100

    InputField: refactor styled components to use theme

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -6,12 +6,12 @@ import { useContentCopy } from '../hooks';
 
 const Input = styled.input`
   border: unset;
-  border-bottom: 1px solid black;
-  padding: 0.5em;
+  border-bottom: 1px solid ${({ theme }) => theme.color.black};
+  padding: ${({ theme }) => theme.spacing(2)};
 
   :focus {
-    outline: 2px solid #ffbc05;
-    box-shadow: 0 0 0 3px rgba(255, 188, 5, 0.265);
+    outline: 2px solid ${({ theme }) => theme.color.primary};
+    box-shadow: 0 0 0 3px ${({ theme }) => theme.color.primaryTransparent};
   }
 `;
 
