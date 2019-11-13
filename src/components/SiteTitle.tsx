d6146ba49555c162b4ed8ba4cce4eac7bb8dd2b5
robertschaap <robert.schaap@mac.com>
2019-11-13 20:06:28 +0100

    SiteTitle: refactor styled components to use theme

--- src/components/SiteTitle.tsx
+++ src/components/SiteTitle.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const SiteTitle = styled.div`
-  font-family: Pattaya, serif;
-  font-size: 1.5em;
+  font-family: ${({ theme }) => theme.typography.fontSecondary};
+  font-size: ${({ theme }) => theme.spacing(6)};
 `;
 
 export default SiteTitle;
